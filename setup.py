278fdd81798914c25cbf2f87234fe79aaa9edb79
Christian Wyglendowski <christian@dowski.com>
2010-09-13 16:27:02 -0400

    Added some metadata to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version="0.1.2",
     packages=['xlsxcessive'],
     author="Christian Wyglendowski",
+    author_email="christian@dowski.com",
+    url="http://xlsx.dowski.com/",
     license="MIT",
     install_requires = [
         'openpack',
