8dc177ee14775d488e09b22067b65d738b7c2e21
Christian Wyglendowski <christian@dowski.com>
2010-09-21 10:04:19 -0400

    Added a description to the setup.py.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name="XlsXcessive",
     version="0.1.3",
+    description="A Python library for writing .xlsx files.",
     packages=['xlsxcessive'],
     author="Christian Wyglendowski",
     author_email="christian@dowski.com",
