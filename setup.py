cf5cf3cc51fa7a3a11db471a58617d0781859460
Christian Wyglendowski <christian@dowski.com>
2010-09-21 09:49:51 -0400

    version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="XlsXcessive",
-    version="0.1.2",
+    version="0.1.3",
     packages=['xlsxcessive'],
     author="Christian Wyglendowski",
     author_email="christian@dowski.com",
