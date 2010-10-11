3e018790d886133522b5c9d541ca7f26e0584a6c
Christian Wyglendowski <christian@dowski.com>
2010-10-11 09:59:26 -0400

    Version 0.1.5 - now with date and time support thanks to Sven.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="XlsXcessive",
-    version="0.1.4",
+    version="0.1.5",
     description="A Python library for writing .xlsx files.",
     packages=['xlsxcessive'],
     author="Christian Wyglendowski",
