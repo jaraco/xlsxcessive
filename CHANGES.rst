a95c428992565a1f95e081b07264773c4a6859ea
Christian Wyglendowski <christian@dowski.com>
2010-11-15 13:21:17 -0500

    Preparing for a new release with performance improvements from Sven.

---
+++ CHANGES.rst
@@ -0,0 +1,9 @@
+XlsXcessive - Changes
+=====================
+
+
+0.1.6 - 2010/11/15
+------------------
+
+* Performance improvements (memory and speed)
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="XlsXcessive",
-    version="0.1.5",
+    version="0.1.6",
     description="A Python library for writing .xlsx files.",
     packages=['xlsxcessive'],
     author="Christian Wyglendowski",
