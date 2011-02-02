21a3de77b3a91f09351a9db043b41a23e904deba
Chris Mulligan <chris.mulligan@gmail.com>
2011-02-02 01:08:21 -0500

    Experimenting with building the save function into the workbook class directly as well

--- xlsxcessive/xlsx.py
+++ xlsxcessive/xlsx.py
@@ -1,31 +1,4 @@
 from xlsxcessive import markup
-from xlsxcessive.workbook import Workbook
-from xlsxcessive.parts import WorkbookPart, WorksheetPart, StylesPart
-from openpack.officepack import OfficePackage
+from xlsxcessive.workbook import Workbook, save
 
-
-def save(workbook, filename, stream=None):
-    """Save the given workbook with the given filename.
-
-    If stream is provided and is a file-like object the .xlsx data
-    will be written there instead.
-    """
-    pack = OfficePackage()
-    wbp = WorkbookPart(pack, '/workbook.xml', data=str(workbook))
-    pack.add(wbp)
-    pack.relate(wbp)
-
-    ##print workbook.stylesheet
-    stp = StylesPart(pack, '/styles.xml', data=str(workbook.stylesheet))
-    pack.add(stp)
-    wbp.relate(stp)
-
-    for i, worksheet in enumerate(workbook.sheets):
-        wid = i + 1
-        wsp = WorksheetPart(pack, "/worksheet%d.xml" % wid, data=str(worksheet))
-        ##print '------'
-        ##print worksheet
-        pack.add(wsp)
-        wbp.relate(wsp, id=worksheet.relation_id)
-    pack.save(stream or filename)
 
