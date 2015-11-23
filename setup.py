005dc64672b9737567eeae48efeea814220fcd9d
Jason R. Coombs <jaraco@jaraco.com>
2015-11-23 14:17:46 -0500

    Update project metadata

--- setup.py
+++ setup.py
@@ -2,16 +2,23 @@ from setuptools import setup
 
 setup(
     name="XlsXcessive",
-    version="0.1.6",
+    version="0.2.0",
     description="A Python library for writing .xlsx files.",
     packages=['xlsxcessive'],
     author="Christian Wyglendowski",
     author_email="christian@dowski.com",
-    url="http://xlsx.dowski.com/",
-    download_url="http://bitbucket.org/dowski/xlsxcessive/downloads",
-    license="MIT",
+    maintainer="Jason R. Coombs",
+    maintainer_email="jaraco@jaraco.com",
+    url="https://bitbucket.org/jaraco/xlsxcessive",
     install_requires = [
         'openpack',
         'six',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
     ],
 )
